3510167252b382ba0aad59a8650e4f380344f23b
ewotoolejc <ewotoolejc@gmail.com>
2023-03-17 17:46:10 -0400

    Day 1 Fixiixing HTML, CSS, and adding JS code

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 	/*----- constants -----*/
+const colors = {
+    'null' : 'white',
+    '1' : 'red',
+    '2' :  'black',
+    '3' : 'gold',
+};
 
 
 	/*----- state variables -----*/
+let board;
+let winner;
 
 
 	/*----- cached elements  -----*/
@@ -14,7 +22,44 @@
 init();
 
 function init() {
+board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null],
 
+winner = null;
+render();
+};
 
+const getCompAnswer = () => {
+    const choices = [];
+    for (let i = 0; i < 4; i++) {
+    choices.push(Math.floor(Math.random() * 3 + 1));
+    }
+    return choices;
+};
 
+let compChoice = getCompAnswer();
+console.log(compChoice);
+
+let colorSelect(evt) {
+    
 }
+
+
+function render() {
+renderBoard();
+}
+
+function renderBoard() {
+    board.forEach(function(pegVal, idx) {
+      const pegEl = document.getElementById(`sq-${idx}`);
+      pegEl.style.backgroundColor = colors[pegVal];
+    });
+}
+
+
+
