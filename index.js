66310c78661e946038955752175a7b1d431a41da
ewotoolejc <ewotoolejc@gmail.com>
2023-03-17 13:49:20 -0400

    Day 1 HTML and  CSS

---
+++ index.js
@@ -0,0 +1,20 @@
+	/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+
+
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
+init();
+
+function init() {
+
+
+
+}
