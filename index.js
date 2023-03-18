8470a02b5e8ac4e67db7c1015a90f0ba4440ee88
ewotoolejc <ewotoolejc@gmail.com>
2023-03-18 16:47:58 -0400

    Mid Day 2: Some CSS Updates and Big JS Updates for picking color on peg, updating table with choices and moving through guess  check on rows

--- index.js
+++ index.js
@@ -1,51 +1,93 @@
 	/*----- constants -----*/
 // const colors = {
-//     'null' : 'white',
+//     '0' : 'white',
 //     '1' : 'red',
 //     '2' :  'black',
 //     '3' : 'gold',
 // };
 
-const colors = ['white', 'red', 'black', 'gold'];
+const colors = ['white', 'rgb(154, 27, 47)', 'black', 'rgb(168, 146, 98)'];
 
 
 	/*----- state variables -----*/
 let board;
 let winner;
-
+let currentColorIndex = -1;
+let countButtonEnterClicks = 0;
+let chkGuess;
 
 	/*----- cached elements  -----*/
 const divs = [...document.querySelectorAll('div')];
-const h2 = document.querySelector('h2');
+// const h2 = document.querySelector('h2');
+
 
 	/*----- event listeners -----*/
-document.getElementById('Enter').addEventListener('click', colorSelect(), false);
+const plyAgn = document.getElementById('Reset').addEventListener('click', init);
+document.querySelector('board').addEventListener('click', (evt) => {
+    const idx = divs.indexOf(evt.target);
+    if (idx === -1) return;
+    const nextColorIndex = ++currentColorIndex % colors.length;
+     evt.target.style.backgroundColor = colors[nextColorIndex];
+});
+
+const enter = document.getElementById('Enter').addEventListener('click', () => {
+    board.forEach(function(pegVal, idx) {
+        const pegEl = document.getElementById(`sq-${idx}`);
+        let col = pegEl.style.backgroundColor;
+        console.log(col);
+        board[idx] = colors.indexOf(col);
+    });
+    if (countButtonEnterClicks === 0) {
+    rowMv = boardMvGen(board, 4);
+   let chkGuess = rowMv.next();
+   countButtonEnterClicks += 1;
+   console.log(chkGuess);
+   return chkGuess;
+            // rowMv.next());
+    // return rowMv.next();
+    } 
+    else if (countButtonEnterClicks >= 1){
+    let chkGuess = rowMv.next();
+    countButtonEnterClicks += 1;
+    console.log(chkGuess);
+    return chkGuess;
+            // rowMv.next());
+    // // return rowMv.next() ;
+    }
+});
 
 	/*----- functions -----*/
 init();
 
 function init() {
-board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ;
+// [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
     // [null, null, null, null],
     // [null, null, null, null],
     // [null, null, null, null],
     // [null, null, null, null],
     // [null, null, null, null],
     // [null, null, null, null],
-
+    // [[0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+//     [0, 0, 0, 0],
+// ]
 winner = null;
 render();
 };
 
 function render() {
     renderBoard();
-    }
+}
     
-    function renderBoard() {
-        board.forEach(function(pegVal, idx) {
-          const pegEl = document.getElementById(`sq-${idx}`);
-          pegEl.style.backgroundColor = colors[idx]; // <- update here!
-        });
+function renderBoard() {
+    board.forEach(function(pegVal, idx) {
+    const pegEl = document.getElementById(`sq-${idx}`);
+    pegEl.style.backgroundColor = colors[pegVal]; 
+    });
 }
 
 
@@ -60,42 +102,10 @@ const getCompAnswer = () => {
 let compChoice = getCompAnswer();
 console.log(compChoice);
 
-// const setColor = n => {
-//     const pegEl = document.getElementById(`sq-${idx}`);
-//     pegEl.style.backgroundColor = colors[n];
-// }
-
-// function* getColor() {
-//     for (let i = 0; i < colors.length; i ++) {
-//       yield i;
-//     }
-// }
-
-
-// function colorSelect(evt) {
-// //    const num = parseInt(evt.target.id.replace('sq-', '')); 
-//    const color = colors[getColor()];
-//    console.log(color);
-//    evt.target.style.backgroundColor = colors[color];
- 
-// }
-
-// let currentColorIndex = -1;
-
-// function colorSelect(evt) {
-//     const nextColorIndex = ++currentColorIndex % colors.length;
-//     console.log(nextColorIndex)
-//     evt.target.backgroundColor = colors[nextColorIndex];
-// }
-
-
-
-function colorSelect(count = 0) {
-    return function () {
-        if (count < colors.length) {
-        h2.style.color = colors[count];
-        console.log(count);
-        ++count;
-      }
+function* boardMvGen(board, rowSize) {
+    for (let index = 0; index < board.length; index += rowSize) {
+    yield board.slice(index, index + rowSize);
     }
-  }
+}
+
+let rowMv = boardMvGen;
