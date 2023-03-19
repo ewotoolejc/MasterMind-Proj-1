51252c3d13511c926496abb4b01e8ab66d3108ec
ewotoolejc <ewotoolejc@gmail.com>
2023-03-19 12:19:08 -0400

    Day 3 mid day: Got win/loss base logic working from comparison of plyr and answer arrays

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const colors = ['white', 'rgb(154, 27, 47)', 'black', 'rgb(168, 146, 98)'];
 
 	/*----- state variables -----*/
 let board;
-let winner;
 let currentColorIndex = -1;
 let countButtonEnterClicks = 0;
+
 
 	/*----- cached elements  -----*/
 const divs = [...document.querySelectorAll('div')];
@@ -36,7 +36,7 @@ document.querySelector('board').addEventListener('click', (evt) => {
        // console.log(col);
     board[idx] = colors.indexOf(col);
     });
-    winner = getWinner();
+    getWinner();
 });
 
 
@@ -45,7 +45,6 @@ init();
 
 function init() {
 board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ;
-winner = null;
 render();
 };
 
@@ -100,14 +99,26 @@ function chkGuess() {
     }
 };
 
+function equalsCheck(pg, ca) {
+    let result = null;
+    for (let i = 0; i < ca.length; i++)
+    if (pg[i] !== ca[i]) {
+    return false;
+    } else {
+    return true;
+    }
+    return result;
+};
 
 function getWinner() {
-    if (cGuess === compChoice) {
+    let plyrGuess = chkGuess();
+    let winner = equalsCheck(plyrGuess.value, compChoice);
+    console.log(winner);
+    if (winner === true) {
         h2.innerHTML = 'You win!!!'
-        return 'true';
-    } if (cGuess[0] === compChoice[0]) {
-        h2.innerHTML = 'You got postiion 1 right but try again!';  
-    }
+    // } if (cGuess[0] === compChoice[0]) {
+    //     h2.innerHTML = 'You got postiion 1 right but try again!';  
+    // }
     //  if (chkGuess[1] === compChoice[1]) {
     //     h2.innerHTML = 'You got postiion 2 right but try again!'
     // } if (chkGuess[2] === compChoice[2]) {
@@ -115,4 +126,61 @@ function getWinner() {
     // } if (chkGuess[3] === compChoice[3]) {
     //     h2.innerHTML = 'You got postiion 2 right but try again!'
     // }
+    } else {
+        h2.innerHTML = 'Nah'
+    }
 }
+
+
+// recursion study for comparing all values
+// const c = [1, 2, 3]
+// const d = [2, 2, 3]
+
+// const equalsCheck = (a, b) => {
+//     const keys = Object.keys(a);
+//     return keys.every(k => equalsCheck(a[k], b[k]));
+// };
+
+// if (equalsCheck(c, d))
+//     console.log("The arrays have the same elements.");
+// else
+//     console.log("The arrays have different elements.");
+
+// const equalsCheck = (a, b) => {
+//     const keys = Object.keys(a);
+//     return keys.every(k => equalsCheck(a[k], b[k]));
+// };
+// let winner = equalsCheck(plyrGuess.value, compChoice);
+
+// function equalsCheck(a, b) {
+//     // check the length
+//     if (a.length != b.length) {
+//         return false;
+//     } else {
+//         let result = false;
+
+//     //     // comparing each element of array 
+//         for (let i = 0; i < a.length; i++) {
+
+//             if (a[i] !== b[i]) {
+//                 return false;
+//             } else {
+//                 result = true;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+// // Declaring arrays
+// const a = [1, 2, 3];
+// const b = [2, 2, 3];
+
+// // Comparing both the arrays
+// const result = equalsCheck(a, b);
+
+// // If the result is true
+// if (result)
+//     console.log("The arrays have the same elements.");
+// else
+//     console.log("The arrays have different elements.");
